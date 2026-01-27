53ff92ad323b9fcf5f7f7f4d9c16ec3e1176613a
Nishant Kumar <nishant098097@gmail.com>
2026-01-27 13:44:52 +0530

    Day 4

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -3,10 +3,18 @@ using namespace std;
 int main(){
     int n = 5;
     int arr[n] = {4,1,5,2,3};
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n-1;j++){
+    //         if(arr[j]>arr[j+1]){
+    //             swap(arr[j],arr[j+1]);
+    //         }
+    //     }
+    // }
+
     for(int i=0;i<n;i++){
         for(int j=0;j<n-1;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+            if(arr[i]<arr[j]){
+                swap(arr[i],arr[j]);
             }
         }
     }
