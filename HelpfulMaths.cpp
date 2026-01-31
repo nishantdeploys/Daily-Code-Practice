8932e3f74b81b65fe1b365b81800874171e70073
Nishant Kumar <nishant098097@gmail.com>
2026-01-31 12:48:03 +0530

    CodeForeces

--- HelpfulMaths.cpp
+++ HelpfulMaths.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 int main(){
     string s;
-    cin>>s;
+    cin >> s;
 
     vector<char> ch;
-    for(int i=0;i<s.length();i+=2){
+    for(int i=0 ; i<s.length() ; i+=2){
         ch.push_back(s[i]);
     }
 
     sort(ch.begin(),ch.end());
 
-    for(int i=0;i<ch.size();i++){
-        cout<<ch[i];
+    for(int i=0 ; i<ch.size() ; i++){
+        cout << ch[i];
         if(i != ch.size()-1){
-            cout<<"+";
+            cout << "+";
         }
     }
 }
